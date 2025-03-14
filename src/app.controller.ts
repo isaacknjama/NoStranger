9533fd9aaaeb0c0919aa8dc08344801ea122ee29
Isaack Njama <isaacknjama@proton.me>
2025-03-14 16:38:45 +0300

    feat(app): add protected route example

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
 import { AppService } from './app.service';
+import { JwtGuard } from './auth/jwt.guard';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,14 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('protected')
+  @UseGuards(JwtGuard)
+  getProtected(@Request() req) {
+    return {
+      message: 'This is a protected route',
+      user: { pubkey: req.user.pubkey },
+    };
   }
 }
